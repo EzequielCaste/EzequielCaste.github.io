c564bb710bc54b4198a08b46fc3de348b5379059
EzequielCaste <ezequiel.caste@gmail.com>
2020-10-21 20:36:15 -0300

    One page portfolio with contact form

---
+++ build/precache-manifest.85f62a162308eeebf40c8fd38fc0ac66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7bee4022c3378030fccfc1b5f551e13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dce5ed177ef99fa50e15",
+    "url": "/static/css/main.cf04b1ff.chunk.css"
+  },
+  {
+    "revision": "ffab22e108838ecf47ec",
+    "url": "/static/js/2.b128c152.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.b128c152.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dce5ed177ef99fa50e15",
+    "url": "/static/js/main.9027504e.chunk.js"
+  },
+  {
+    "revision": "b757cbfa5b52942fc711",
+    "url": "/static/js/runtime-main.e327d292.js"
+  }
+]);
