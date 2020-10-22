619674f0caac1b3409a6e6bd428d7e0036c84acf
EzequielCaste <ezequiel.caste@gmail.com>
2020-10-21 21:48:55 -0300

    Deploy to Github Pages

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.85f62a162308eeebf40c8fd38fc0ac66.js"
+  "/precache-manifest.5bb33e4fbd6c8532c61f397605a90833.js"
 );
 
 self.addEventListener('message', (event) => {
